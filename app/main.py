81f6ab232d76714b3eb51ef3e5f8c18bcd224095
Viktor Ilin <viktor125142@gmail.com>
2024-06-13 19:53:44 +0800

    add a 500 handler

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
 from fastapi import FastAPI
 from fastapi import Request
 
+
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 from app.operations.routers import router_calculate
 
-app = FastAPI()
+app = FastAPI(title="Calculate deposit")
 
 
 @app.exception_handler(RequestValidationError)
-def validation_exception_handler(request: Request,
-                                 exc: RequestValidationError):
+def validation_exception_handler(
+        request: Request,
+        exc: RequestValidationError
+):
     return JSONResponse(
         status_code=400,
         content={"detail": exc.errors(), "body": exc.body},
+    )
+
+
+@app.exception_handler(Exception)
+def internal_server_error_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal Server Error", "error": str(exc)},
     )
 
 
