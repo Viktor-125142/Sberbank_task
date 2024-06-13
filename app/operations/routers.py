1e03210cbcbb29a793ea91b60fcd7819c9b18526
Viktor Ilin <viktor125142@gmail.com>
2024-06-13 19:56:02 +0800

    delete block try

--- app/operations/routers.py
+++ app/operations/routers.py
@@ -1,25 +1,23 @@
 from fastapi import APIRouter, HTTPException
 from typing import List, Dict
 
-
 from app.operations.schemas import DepositRequest
 from app.operations.services import calculate_deposit
 
 router_calculate = APIRouter(
-    prefix="/calculate",
+    prefix="/deposit/calculate",
     tags=["calculate_deposit"]
 )
 
 
 @router_calculate.post("",
                        response_model=List[Dict[str, float]],
-                       status_code=200
+                       status_code=200,
+                       summary="Calculate deposit",
+                       description="Calculate deposit based on input parameters"
                        )
 def calculate_endpoint(
         request: DepositRequest
 ) -> List[Dict[str, float]]:
-    try:
-        result = calculate_deposit(request)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    result = calculate_deposit(request)
+    return result
